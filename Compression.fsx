c4946ff688770c5a86eead483b5a4e2216ac6d15
ingted <anibal.yeh@gmail.com>
2024-10-23 11:01:32 +0800

    init

---
+++ Compression.fsx
@@ -0,0 +1,28 @@
+﻿namespace fs
+
+open System.IO
+open System.IO.Compression
+open System.Text
+
+module Deflate =
+    
+    let compress (bytes: byte[]) : byte[] =
+        use msi = new MemoryStream(bytes)
+        use mso = new MemoryStream()
+        use gs = new DeflateStream(mso, CompressionMode.Compress)
+        msi.CopyTo(gs)
+        gs.Close()  // 确保压缩流已关闭
+        mso.ToArray()
+
+    let decompress (bytes: byte[]) : byte[] =
+        use msi = new MemoryStream(bytes)
+        use mso = new MemoryStream()
+        use gs = new DeflateStream(msi, CompressionMode.Decompress)
+        gs.CopyTo(mso)
+        mso.ToArray()
+
+    let compressString (str: string) : byte[] =
+        compress (Encoding.UTF8.GetBytes(str))
+
+    let decompressToString (bytes: byte[]) : string =
+        Encoding.UTF8.GetString(decompress(bytes))
---
+++ PostBuildEvent.ps1
@@ -0,0 +1,14 @@
+﻿param(
+    $assembly
+)
+write-host ($assembly + ': Current path: ' + (pwd).path)
+cd ./bin
+try {
+    $pkg = (dir "$($assembly)*.nupkg" | Sort-Object -Property Name -Descending)[0].Name
+    invoke-expression "dotnet nuget push $pkg --api-key $(gc G:\Nuget\apikey.txt) --source https://api.nuget.org/v3/index.json  --skip-duplicate"
+}
+catch {
+    write-host "=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+="
+    write-host $_
+    write-host "=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+="
+}
